d090a053c09bfe5c6f25896d3a313201fef4b740
Kamil Nadlonek <kamilnadlonek@gmail.com>
2017-08-23 19:38:22 +0200

    Dispaly event content

--- src/main/java/com/kn/elephant/note/ui/event/EventPanel.java
+++ src/main/java/com/kn/elephant/note/ui/event/EventPanel.java
@@ -1,11 +1,16 @@
 package com.kn.elephant.note.ui.event;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.kn.elephant.note.dto.EventContentDto;
+import com.kn.elephant.note.dto.EventDto;
 import com.kn.elephant.note.ui.setting.TitlePanel;
 
 import javafx.geometry.Pos;
 import javafx.scene.Node;
+import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
-import javafx.scene.text.Text;
 
 /**
  * Created by Kamil Nadłonek on 20-08-2017
@@ -13,16 +18,25 @@ import javafx.scene.text.Text;
  */
 public class EventPanel extends TitlePanel {
 
-	public EventPanel() {
-		setTitle("Event name");
-		setContent(createContent());
-	}
+    protected void loadEvent(EventDto eventDto) {
+        setTitle(eventDto.getName());
+        setContent(createContent(eventDto));
+    }
 
-	private Node createContent() {
-		HBox box = new HBox();
-		Text text = new Text("Content Event ....");
-		box.getChildren().addAll(text);
-		box.setAlignment(Pos.TOP_CENTER);
-		return box;
-	}
+    private Node createContent(EventDto eventDto) {
+        HBox box = new HBox();
+        List<Node> collect = eventDto.getContent().stream().map(this::create).collect(Collectors.toList());
+        box.getChildren().addAll(collect);
+        box.setAlignment(Pos.TOP_CENTER);
+        return box;
+    }
+
+    private Node create(EventContentDto dto) {
+        HBox box = new HBox();
+        Label date = new Label(dto.getDate().toString());
+        Label text = new Label(dto.getContent());
+        box.getChildren().addAll(date, text);
+
+        return box;
+    }
 }
