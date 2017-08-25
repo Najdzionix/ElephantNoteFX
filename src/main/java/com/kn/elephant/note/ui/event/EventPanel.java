3179c9b691f3a80d43b71a2bfbc8a8a3622714d6
Kamil Nadlonek <kamilnadlonek@gmail.com>
2017-08-25 18:31:45 +0200

    Add Event dialog beta

--- src/main/java/com/kn/elephant/note/ui/event/EventPanel.java
+++ src/main/java/com/kn/elephant/note/ui/event/EventPanel.java
@@ -1,30 +1,79 @@
 package com.kn.elephant.note.ui.event;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.chrono.ChronoLocalDate;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
+import com.google.inject.Inject;
+import com.kn.elephant.note.Main;
 import com.kn.elephant.note.dto.EventContentDto;
 import com.kn.elephant.note.dto.EventDto;
-import com.kn.elephant.note.ui.setting.TitlePanel;
+import com.kn.elephant.note.service.EventService;
+import com.kn.elephant.note.ui.BasePanel;
+import com.kn.elephant.note.ui.UIFactory;
+import com.kn.elephant.note.utils.Icons;
 
+import de.jensd.fx.glyphs.materialdesignicons.MaterialDesignIcon;
+import javafx.application.Platform;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.DateCell;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Dialog;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.util.Callback;
+import jfxtras.scene.control.LocalTimeTextField;
+import lombok.extern.log4j.Log4j2;
 
 /**
  * Created by Kamil Nadłonek on 20-08-2017
  * email:kamilnadlonek@gmail.com
  */
-public class EventPanel extends TitlePanel {
+@Log4j2
+public class EventPanel extends BasePanel {
 
-    protected void loadEvent(EventDto eventDto) {
-        setTitle(eventDto.getName());
-        setContent(createContent(eventDto));
+	@Inject
+	private EventService eventService;
+
+	private DatePicker datePicker;
+	private LocalTimeTextField calendarTextField;
+
+	protected void loadEvent(EventDto eventDto) {
+		Button addEvent = new Button();
+		Icons.addIcon(MaterialDesignIcon.PLUS_CIRCLE, addEvent, "2.5em");
+		BorderPane box = new BorderPane();
+        Label name = new Label(eventDto.getName());
+        addEvent.setOnAction(event -> {
+            Dialog<EventDto> dialog = addEventDialog();
+            Optional<EventDto> newEventDto = dialog.showAndWait();
+            if (newEventDto.isPresent()) {
+                log.info("Save new event: {}", newEventDto);
+                eventService.saveEvent(newEventDto.get());
+            }
+        });
+		box.getStyleClass().addAll("custom-pane");
+		box.setCenter(name);
+		box.setRight(addEvent);
+		setTop(box);
+        setCenter(createContent(eventDto));
     }
 
     private Node createContent(EventDto eventDto) {
         HBox box = new HBox();
+        box.getStyleClass().addAll("custom-pane");
         List<Node> collect = eventDto.getContent().stream().map(this::create).collect(Collectors.toList());
         box.getChildren().addAll(collect);
         box.setAlignment(Pos.TOP_CENTER);
@@ -39,4 +88,89 @@ public class EventPanel extends TitlePanel {
 
         return box;
     }
+
+
+    private Dialog<EventDto> addEventDialog() {
+		Dialog<EventDto> dialog = new Dialog<>();
+		dialog.setTitle("Create event wizard");
+		dialog.setHeaderText("Please give name and date start event and also if should be repeat....");
+		dialog.setResizable(false);
+		dialog.getDialogPane().getStyleClass().add("card");
+
+		Label nameLabel = UIFactory.createLabel("Name: ");
+
+		Label repeatLabel = UIFactory.createLabel("Should repeat?: ");
+		TextField nameTextField = new TextField();
+		createStartDate();
+		CheckBox repeat = new CheckBox();
+		Platform.runLater(nameTextField::requestFocus);
+
+		VBox box = new VBox();
+		box.getChildren().addAll( nameLabel, nameTextField, createStartDate(), repeatLabel, repeat);
+
+		dialog.getDialogPane().setContent(box);
+
+		ButtonType buttonTypeOk = new ButtonType("Okay", ButtonBar.ButtonData.OK_DONE);
+
+		dialog.getDialogPane().getButtonTypes().add(buttonTypeOk);
+		final Button btOk = (Button) dialog.getDialogPane().lookupButton(buttonTypeOk);
+		btOk.getStyleClass().add("button-action");
+//		btOk.addEventFilter(ActionEvent.ACTION, event -> {
+//			if (!validatorHelper.isValid()) {
+//				event.consume();
+//			}
+//		});
+
+		dialog.setResultConverter(buttonType -> {
+			if (buttonType == buttonTypeOk) {
+				LocalDateTime time = datePicker.getValue().atTime(calendarTextField.getLocalTime());
+				EventDto eventDto = new EventDto().setName(nameTextField.getText())
+					.setStartDate(time);
+
+				return eventDto;
+			}
+			return null;
+		});
+
+
+		dialog.getDialogPane().getStylesheets().addAll(Main.loadCssFiles());
+		return dialog;
+	}
+
+
+	private Node createStartDate() {
+		datePicker = new DatePicker();
+        Label dateLabel = UIFactory.createLabel("Date: ");
+        final Callback<DatePicker, DateCell> dayCellFactory = new Callback<DatePicker, DateCell>() {
+            @Override
+            public DateCell call(final DatePicker datePicker1) {
+                return new DateCell() {
+                    @Override
+                    public void updateItem(LocalDate item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if (item.isBefore(ChronoLocalDate.from(LocalDate.now()))) {
+                            setDisable(true);
+                            setStyle("-fx-background-color: #ffc0cb;");
+                        }
+                    }
+                };
+            }
+        };
+        datePicker.setDayCellFactory(dayCellFactory);
+        Label hourLabel = UIFactory.createLabel("Time:");
+		calendarTextField = new LocalTimeTextField();
+		calendarTextField.setLocalTime(LocalTime.now());
+		calendarTextField.getStyleClass().add("textFieldTime");
+		GridPane pane  = new GridPane();
+		pane.setHgap(20);
+		calendarTextField.setStyle("-fx-padding: 7 0 0 0;");
+		GridPane.setConstraints(dateLabel,0,0);
+		GridPane.setConstraints(hourLabel,1,0);
+		GridPane.setConstraints(datePicker,0,1);
+		GridPane.setConstraints(calendarTextField,1,1);
+
+		pane.getChildren().addAll(dateLabel, datePicker, hourLabel, calendarTextField);
+
+        return pane;
+	}
 }
