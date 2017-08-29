5d501bd78e0e4865c7da8c5470f01aba9de66f3a
Kamil Nadlonek <kamilnadlonek@gmail.com>
2017-08-29 18:54:04 +0200

    Events fix edit and refresh event list after updated/create event

--- src/main/java/com/kn/elephant/note/ui/event/ListEvents.java
+++ src/main/java/com/kn/elephant/note/ui/event/ListEvents.java
@@ -3,39 +3,49 @@ package com.kn.elephant.note.ui.event;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.controlsfx.control.action.ActionMap;
+import org.controlsfx.control.action.ActionProxy;
+
 import com.google.inject.Inject;
 import com.kn.elephant.note.dto.EventDto;
 import com.kn.elephant.note.service.EventService;
 import com.kn.elephant.note.ui.BasePanel;
 
-import javafx.scene.Node;
+import javafx.event.ActionEvent;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import lombok.extern.log4j.Log4j2;
 
 /**
  * Created by Kamil Nadłonek on 20-08-2017
  * email:kamilnadlonek@gmail.com
  */
+@Log4j2
 public class ListEvents extends BasePanel {
 
 	private static final String ACTIVE_CLASS = "active";
 	@Inject
 	private EventService  eventService;
-	private List<Node> allEventNodes;
+	private List<Button> allEventNodes;
 	private VBox menuBox;
 	private EventPanel eventPanel;
+	private List<EventDto> allEvents;
 
 	public ListEvents(EventPanel eventPanel){
+		ActionMap.register(this);
 		this.eventPanel = eventPanel;
+
 		createContent();
+		setActiveEvent(null);
 		getStyleClass().addAll("menu-left", "events");
 	}
 
 	private void createContent() {
 		menuBox = new VBox();
 		menuBox.getStyleClass().add("menu-panel");
-		List<EventDto> allEvents = eventService.getAllEvents();
+		allEvents = eventService.getAllEvents();
 		allEventNodes = allEvents.stream().map(this::createEvent).collect(Collectors.toList());
 		menuBox.getChildren().addAll(allEventNodes);
 
@@ -43,14 +53,28 @@ public class ListEvents extends BasePanel {
 		borderPane.getChildren().add(menuBox);
 		AnchorPane.setTopAnchor(menuBox, 0.0);
 		AnchorPane.setBottomAnchor(menuBox, 0.0);
-
 		setCenter(borderPane);
-        eventPanel.loadEvent(allEvents.get(0));
     }
 
-    private Node createEvent(EventDto eventDto) {
+    private void setActiveEvent(EventDto eventDto) {
+        if (eventDto != null) {
+            eventPanel.loadEvent(eventDto);
+            allEventNodes.forEach(node -> {
+                if (eventDto.getId().toString().equals(node.getId())) {
+                    node.getStyleClass().add(ACTIVE_CLASS);
+                }
+            });
+        } else {
+        	eventPanel.loadEvent(allEvents.get(0));
+        	allEventNodes.get(0).getStyleClass().add(ACTIVE_CLASS);
+		}
+    }
+
+    private Button createEvent(EventDto eventDto) {
         Button button = new Button(eventDto.getName());
-        button.getStyleClass().add("menu-cell");
+        button.setId(eventDto.getId().toString());
+        button.setAlignment(Pos.CENTER);
+        button.getStyleClass().addAll("menu-cell", "event-button");
         button.setOnAction(event -> {
             eventPanel.loadEvent(eventDto);
             allEventNodes.forEach(node -> node.getStyleClass().removeAll(ACTIVE_CLASS));
@@ -58,4 +82,12 @@ public class ListEvents extends BasePanel {
         });
         return button;
     }
+
+    @ActionProxy(text = "")
+	private void refreshEventList(ActionEvent event) {
+		log.info("Refresh events list");
+		EventDto eventDto = (EventDto) event.getSource();
+		createContent();
+		setActiveEvent(eventDto);
+	}
 }
